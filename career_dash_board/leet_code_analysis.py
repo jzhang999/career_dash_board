8f0be4261af362376079550e98c6d0f685718de5
sukeascat <67693477+sukeascat@users.noreply.github.com>
2022-04-27 19:04:42 -0400

    Update leet_code_analysis.py

--- career_dash_board/leet_code_analysis.py
+++ career_dash_board/leet_code_analysis.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
 
-def get_leetcode_data(dataFrame):
+def get_leetcode_data(dataFrame, company_name):
 
     count_dict = {}
     for idx in dataFrame.index:
         company = dataFrame['Company'][idx]
+        if company != company_name:
+            continue
         question = dataFrame["Question"][idx].replace(" ", '\n')
 
         if question in count_dict.keys():
@@ -21,6 +23,12 @@ def get_leetcode_data(dataFrame):
     question_list = list(sorted_dict.keys())[:5]
     count_list = list(sorted_dict.values())[:5]
 
+    print("\nThe top5 questions in " + company_name + " are:")
+    i = 1
+    for question in question_list:
+        new_question_string = question.replace("\n", ' ')
+        print(str(i) + ". " + new_question_string , end = "\n")
+        i += 1
     ax = plt.axes()
     plt.bar(range(len(count_list)), count_list, tick_label=question_list)
     plt.show()
